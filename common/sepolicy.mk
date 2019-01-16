e41171151660dbe4bc1e9c4645e74e002910137b
mracar07 <umutcan@umutcanacar.me>
2019-01-16 11:30:24 +0100

    AOSAP-ify
    Signed-off-by: mracar07 <umutcan@umutcanacar.me>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from Aosap
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/aosap/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/aosap/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/aosap/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from aosap
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/aosap/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/aosap/sepolicy/qcom/common \
+    device/aosap/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
